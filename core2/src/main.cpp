87af82e660405d63dfea367b98eb5d8a8d38ef62
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-10 11:29:47 -0500

    Added script to connect to wifi

--- core2/src/main.cpp
+++ core2/src/main.cpp
@@ -1,10 +1,43 @@
 #include <Arduino.h>
-#include <WiFi.h>
+#include <ArduinoJson.h>
 #include <M5Core2.h>
+#include <WiFi.h>
+
+String WiFiPrefix = "Omega";
+char WiFiPassword[] = "123456789";
+
+
+int checkWiFi() {
+	if(!WiFi.isConnected()) {
+
+		int status = WiFi.scanComplete();
+
+		if(status == -2) {
+			WiFi.scanNetworks(true);
+		} else if(status) {
+			for(int i = 0; i < status; i++) {
+				if(WiFi.SSID(i).indexOf(WiFiPrefix) == 0) {
+					WiFi.begin(WiFi.SSID(i).begin(), WiFiPassword);
+				}
+			}
+		}
+
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
 
 void setup() {
+  M5.begin();
+
+  // Begin wifi and search
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
 }
 
-void loop(){
-}
 
+void loop() {
+	checkWiFi();
+}

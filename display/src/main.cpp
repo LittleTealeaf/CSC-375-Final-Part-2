5e2660f373d398de513303d8b7e52018ae0fedcf
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-11 16:35:56 -0500

    Started working on rewriting once again, things happen!

--- display/src/main.cpp
+++ display/src/main.cpp
@@ -13,123 +13,128 @@
 #define SPRITE_HEIGHT 118
 #define SPRITE_PADDING 1
 
-IPAddress server(192,168,4,3);
+IPAddress server(192, 168, 4, 2);
 int port = 1883;
-
-String WiFiPrefix = "Omega";
-char WiFiPassword[] = "123456789";
-int wifiStatus;
-long scanDelay, mqttConnectDelay;
-
-Ticker wifiTicker;
-
-TFT_eSprite screenSprite = TFT_eSprite(&M5.Lcd);
-TFT_eSprite viewPorts[] = {TFT_eSprite(&M5.Lcd), TFT_eSprite(&M5.Lcd),
-                           TFT_eSprite(&M5.Lcd), TFT_eSprite(&M5.Lcd),
-                           TFT_eSprite(&M5.Lcd), TFT_eSprite(&M5.Lcd)};
 
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
+Ticker wifiTicker;
+bool wifiLock;
+
+String WiFiPrefix = "Omega";
+char WiFiPassword[] = "123456789";
+
+TFT_eSprite screen(&M5.Lcd);
+TFT_eSprite viewPorts[] = {TFT_eSprite(&M5.Lcd), TFT_eSprite(&M5.Lcd),
+                           TFT_eSprite(&M5.Lcd), TFT_eSprite(&M5.Lcd),
+                           TFT_eSprite(&M5.Lcd), TFT_eSprite(&M5.Lcd)};
+
+typedef struct Sensor {
+	String mac = "";
+	long lastPingedAt;
+} Sensor;
+
+Sensor sensors[6];
+
 void displayMessage(const char *message) {
-  screenSprite.fillScreen(TFT_BLACK);
-  screenSprite.setTextSize(1);
-  screenSprite.setCursor(0, 0);
-  screenSprite.println(message);
-  screenSprite.pushSprite(0, 0);
+	screen.fillScreen(TFT_BLACK);
+	screen.setCursor(0, 0);
+	screen.print(message);
+	screen.pushSprite(0, 0);
 }
 
 void pushViewPort(int index) {
-  int y = index < 3 ? SPRITE_PADDING : SPRITE_HEIGHT + SPRITE_PADDING * 3;
-  int x = SPRITE_PADDING + (SPRITE_PADDING * 2 + SPRITE_WIDTH) * (index % 3);
+  int x = index < 3 ? SPRITE_PADDING : SPRITE_HEIGHT + SPRITE_PADDING * 3;
+  int y = SPRITE_PADDING + (SPRITE_PADDING * 2 + SPRITE_WIDTH) * (index % 3);
+	viewPorts[index].setCursor(0, 0);
   viewPorts[index].pushSprite(x, y);
 }
 
-void onWifiDisconnected() { displayMessage("Wifi Not Connected"); }
+void clearWifiLock() { wifiLock = false; }
 
-void onWiFiConnected() {
-
-}
-
-bool checkWiFi() {
+bool connectWiFi() {
   int status = WiFi.status();
 
-  if (status == WL_CONNECTED) {
-    if (wifiStatus != status) {
-      onWiFiConnected();
-    }
-  } else {
-    if (status == WL_CONNECTED) {
-      onWifiDisconnected();
-    }
-
-    // if (scanDelay <= millis()) {
+  if (status != WL_CONNECTED && !wifiLock) {
     int scanResult = WiFi.scanComplete();
 
-    if (scanResult == -2 && scanDelay <= millis()) {
+    if (scanResult == -2) {
+			displayMessage("Scanning Wifi Networks");
       WiFi.scanNetworks(true);
-    } else if (scanResult == -1) {
     } else if (scanResult >= 0) {
       for (int i = 0; i < scanResult; i++) {
         if (WiFi.SSID(i).indexOf(WiFiPrefix) == 0) {
+					displayMessage("Connecting to Network");
           WiFi.begin(WiFi.SSID(i).begin(), WiFiPassword);
-          scanDelay = millis() + 10000;
+          wifiLock = true;
+          wifiTicker.once(5, clearWifiLock);
         }
-				WiFi.scanDelete();
       }
-      //}
+			WiFi.scanDelete();
     }
   }
-
-  wifiStatus = status;
 
   return status == WL_CONNECTED;
 }
 
-bool checkMQTT() {
-	
-	bool connected = mqttClient.connected();
+bool connectMQTT() {
 
-	if(!connected && mqttConnectDelay <= millis()) {
-
-		displayMessage("Connecting to MQTT Client");
-
-		if(mqttClient.connect("arduinoClient")) {
+	if(!mqttClient.connected()) {
+		displayMessage("Attempting to Connect to MQTT");
+		if(mqttClient.connect("takwashnak")) {
+			displayMessage("Connected to MQTT");
 			mqttClient.subscribe("CSC375/dist");
-		} else {
-			displayMessage("MQTT Connection Failed, Waiting 10 seconds");
-			mqttConnectDelay = millis() + 10000;
+			return true;
+		}
+		return false;
+	}
+
+	return true;
+}
+
+void onMessageReceived(char *topic, byte *payload, unsigned int length) {
+	DynamicJsonDocument doc(1024);
+	deserializeJson(doc, payload);
+
+	const char* mac = doc["MAC"];
+	int distance = doc["dist"];
+
+	for(int i = 0; i < 6; i++) {
+		if(sensors[i].mac.equals("") || sensors[i].mac.equals(mac)) {
+			sensors[i].mac = mac;
+			sensors[i].lastPingedAt = millis();
+			viewPorts[i].setCursor(0, 0);
+			viewPorts[i].printf("%d",distance);
+			pushViewPort(i);
+			break;
 		}
 	}
 
-
-	return false;	
-}
-
-void onMessageReceived(char* topic, byte* payload, unsigned int length) {
 }
 
 void setup() {
   M5.begin();
-  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  mqttClient.setServer(server, port);
+  mqttClient.setCallback(onMessageReceived);
+
+  wifiLock = false;
 
   for (int i = 0; i < 6; i++) {
     viewPorts[i].setColorDepth(SPRITE_COLOR_DEPTH);
     viewPorts[i].createSprite(SPRITE_WIDTH, SPRITE_HEIGHT);
     viewPorts[i].fillScreen(TFT_DARKGREY);
   }
-  screenSprite.setColorDepth(SPRITE_COLOR_DEPTH);
-  screenSprite.createSprite(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  onWifiDisconnected();
-	mqttClient.setServer(server, port);
-	mqttClient.setCallback(onMessageReceived);
+  screen.setColorDepth(SPRITE_COLOR_DEPTH);
+  screen.createSprite(SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
 void loop() {
-	if(checkWiFi() && checkMQTT()) {
+  if (connectWiFi() && connectMQTT()) {
 		mqttClient.loop();
-	}
+  }
 }

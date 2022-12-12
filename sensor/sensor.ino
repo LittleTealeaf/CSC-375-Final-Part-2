29fb1f8b805647fa1f07ecf7040e6e05d71c9ec9
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-12 14:52:47 -0500

    Removed Commented Code

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -132,29 +132,3 @@ void loop() {
   }
   digitalWrite(LED_BUILTIN, connected && active ? HIGH : LOW);
 }
-
-
-
-// void setup() {
-//   Serial.begin(115200);
-//   delay(100);
-// }
-
-// void loop() {
-//   int16_t  dist, temp;
-//   pinMode(trigechoPin,OUTPUT);
-//   digitalWrite(trigechoPin,LOW);
-
-//   digitalWrite(trigechoPin,HIGH);//Set the trig pin High
-//   delayMicroseconds(10);     //Delay of 10 microseconds
-//   digitalWrite(trigechoPin,LOW); //Set the trig pin Low
-
-//   pinMode(trigechoPin,INPUT);//Set the pin to input mode
-//   pulseWidthUs = pulseIn(trigechoPin,HIGH);//Detect the high level time on the echo pin, the output high level time represents the ultrasonic flight time (unit: us)
-
-//   distance = pulseWidthUs * VELOCITY_TEMP(20) / 2.0;//The distance can be calculated according to the flight time of ultrasonic wave,/
-//                                                     //and the ultrasonic sound speed can be compensated according to the actual ambient temperature
-//   Serial.print(distance, DEC);
-//   Serial.println("cm");
-//   delay(500);
-// }
